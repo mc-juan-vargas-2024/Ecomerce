440d907a4d8c573ce0c5476fda56a47fee99cb80
CSU22-AINF <CSU22@academico.unab.ad>
2025-09-11 11:00:22 -0500

    rutas basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('products', function () {
+    return "LISTADO DE PRODUCTOS";
+});
+Route::get('products/create', function () {
+    return "formulario de creacion de productos";
+});
+Route::get('products/{id}/{category}', function ($id, $category = null) {
+    if ($category == null){
+        return "detalle de cada producto" .$id ;
+    }
+    return "detalle de cada producto" . $id . "de la categoria " .$category;
+});
