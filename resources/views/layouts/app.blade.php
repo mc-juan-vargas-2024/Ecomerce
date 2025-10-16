0f526b461777d44b06658ca0a8dd466d0b9843f9
mc-juan-vargas-2024 <jvargas156@unab.edu.co>
2025-10-16 13:00:52 -0500

    adelanto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    @yield("css")
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,7 +16,9 @@
 
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    
+    <!-- Estilos personalizados -->
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 </head>
 <body>
     <div id="app">
