9021448e7e275e3d7bd0e03c8add0efcd8ea989e
mc-juan-vargas-2024 <jvargas156@unab.edu.co>
2025-11-05 17:18:08 -0500

    merge task

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
-=======
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductContoller;
->>>>>>> task
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class,'index']);
@@ -21,7 +16,6 @@ Route::prefix('products')->controller(ProductController::class)->group(function 
     Route::get('/{id}/{category}',"show");
 });
 
-<<<<<<< HEAD
 
 Route::prefix('admin')->controller(AdminController::class)->group(function () {
     
@@ -37,14 +31,3 @@ Route::prefix('admin')->controller(CategoryController::class)->group(function ()
 });
 
 Auth::routes();
-=======
-Route::prefix("/products")->controller(ProductContoller::class)->group(function (){
-    Route::get('/',"index");
-    Route::get('/create', 'create');
-    Route::get('/{id}/{category}', 'show');
-});
-Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/' , [HomeController::class, 'welcome'] );
->>>>>>> task
