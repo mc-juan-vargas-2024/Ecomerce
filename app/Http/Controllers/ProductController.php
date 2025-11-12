3c43bfba44890c4a75544d89425e439e1824c2d9
mc-juan-vargas-2024 <jvargas156@unab.edu.co>
2025-11-12 18:48:16 -0500

    validacion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,22 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    function index(){
+    function index()
+    {
         return view('products.index');
-    }function create(){
-        $brands=Brand::all();
-        $categories=Category::all();
-         return view('products.create',[
-            'brands'=>$brands,
-            'categories'=>$categories,
-         ]);
     }
-    
-    function show($id,$category=null){
-         return view('products.show');
+    function create()
+    {
+        $brands = Brand::all();
+        $categories = Category::all();
+        return view('products.create', [
+            'brands' => $brands,
+            'categories' => $categories,
+        ]);
+    }
+
+    function show($id, $category = null)
+    {
+        return view('products.show');
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'category' => 'required|exists:category,id',
+            'brand' => 'required|exists:brand,id',
+        ]);
+
+        $product = new Product();
+        $product->name = $request->get('name');
+        $product->description = $request->get('description');
+        $product->price = $request->get('price');
+        $product->category_id = $request->get('category');
+        $product->brand_id = $request->get('brand');
+
+        $product->save();
+        return "Save Product";
     }
 }
